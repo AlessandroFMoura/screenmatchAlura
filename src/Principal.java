2cfa2e800bf19c0eedb226b4600f588df4cbdaef
alessandrofmoura <alessandronahal@gmail.com>
2025-06-05 20:55:31 -0300

    Primeiro commit scrennmatch

---
+++ src/Principal.java
@@ -0,0 +1,59 @@
+import br.com.alura.screenmatch.calculo.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculo.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Series;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+
+        meuFilme.exibiFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total de Avaliaçõe %d: ".formatted(meuFilme.getTotalAvaliacoes()));
+        System.out.println(meuFilme.pegaMedia());
+
+        System.out.println("================================================================");
+
+        Series lost = new Series();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibiFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodioPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração em minuto para maratonar Lost: "+lost.getDuracaoEmMinutos());
+
+        System.out.println("================================================================");
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        System.out.println("================================================================");
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+        System.out.println("================================================================");
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+    }
+}
---
+++ src/br/com/alura/screenmatch/modelos/Series.java
@@ -0,0 +1,47 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Series extends Titulo {
+    private int temporadas;
+    private boolean avita;
+    private int episodioPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAvita() {
+        return avita;
+    }
+
+    public void setAvita(boolean avita) {
+        this.avita = avita;
+    }
+
+    public int getEpisodioPorTemporada() {
+        return episodioPorTemporada;
+    }
+
+    public void setEpisodioPorTemporada(int episodioPorTemporada) {
+        this.episodioPorTemporada = episodioPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+    /**
+     * sobre escreve o metodo da classe principal.
+     */
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodioPorTemporada * minutosPorEpisodio;
+    }
+}
